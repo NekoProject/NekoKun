3097e6d73e53a416ef249b399ae80d46d4298a25
orzFly <wz.bluesnow@gmail.com>
2012-07-28 10:14:57 -0700

    Scintilla Margin Backcolor fix

--- NekoKun/UI/Scintilla/Scintilla.cs
+++ NekoKun/UI/Scintilla/Scintilla.cs
@@ -6,9 +6,14 @@ namespace NekoKun.UI
 {
     public class Scintilla : ScintillaNet.Scintilla
     {
+    	protected System.Drawing.Color back = System.Drawing.Color.FromArgb(191, 219, 255);
+    	
         public Scintilla()
         {
             this.Font = Program.GetMonospaceFont();
+            
+            // left margin backcolor
+			this.Styles[33].BackColor = back;
         }
     }
 }
