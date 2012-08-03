40e89136607cb06493190c2f664c18a5e517f549
orzFly <wz.bluesnow@gmail.com>
2012-08-03 12:26:34 +0800

    Reorganizing file structure
    Signed-off-by: orzFly <wz.bluesnow@gmail.com>

---
+++ NekoKun/Core/Projects/ProjectManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace NekoKun
+{
+    public static class ProjectManager
+    {
+        private static bool isProjectOpened;
+        private static string projectDir;
+        private static string projectFile;
+        private static ProjectDocumentFile projectDocument;
+        private static Dictionary<string, object> components;
+
+        public static string ProjectDir
+        {
+            get { return projectDir; }
+        }
+
+        public static Dictionary<string, object> Components
+        {
+            get { return components; }
+        }
+
+        public static void OpenProject(string file)
+        {
+            if (isProjectOpened)
+                throw new InvalidOperationException("A project has already been opened. ");
+
+            try
+            {
+                projectFile = file;
+                projectDir = System.IO.Path.GetDirectoryName(file);
+                projectDocument = new ProjectDocumentFile(file);
+                components = new Dictionary<string, object>();
+
+                foreach (var item in projectDocument.Components)
+                {
+                    if (item.Attributes["ID"] != null)
+                    {
+                        var dict = new Dictionary<string, object>();
+                        foreach (XmlNode property in item.ChildNodes)
+                        {
+                            if (property.HasChildNodes && property.ChildNodes.Count == 1 && (property.FirstChild is XmlText))
+                                dict.Add(
+                                    property.Attributes["Name"].Value,
+                                    property.FirstChild.Value
+                                );
+                            else
+                                dict.Add(
+                                    property.Attributes["Name"].Value,
+                                    property.ChildNodes
+                                );
+
+                        }
+
+                        try
+                        {
+                            components.Add(
+                                item.Attributes["ID"].Value,
+                                Program.CreateInstanceFromTypeName(item.Attributes["Name"].Value, dict)
+                            );
+                        }
+                        catch (TypeLoadException)
+                        {
+                        }
+
+                    }
+                }
+
+                isProjectOpened = true;
+            }
+            catch (Exception e)
+            {
+                Clean();
+                Program.Logger.Log(Program.ExceptionMessage(e));
+                throw e;
+            }
+        }
+
+        private static void Clean()
+        {
+            isProjectOpened = false;
+            projectDocument = null;
+            projectDir = null;
+            projectFile = null;
+            components = null;
+        }
+    }
+}
