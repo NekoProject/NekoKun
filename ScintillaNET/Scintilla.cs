5f8f509c4d69d73a0a9a76de8e18417d562765f6
orzFly <wz.bluesnow@gmail.com>
2012-08-04 12:11:35 +0800

    Revert "Include support for SciLexer64 and update SciLexer.dll & SciLexer64.dll to 3.0.4"
    This reverts commit 676e8601c3b439c304a06358be56ff5b485cb6ac.
    
    Signed-off-by: orzFly <wz.bluesnow@gmail.com>

--- ScintillaNET/Scintilla.cs
+++ ScintillaNET/Scintilla.cs
@@ -31,7 +31,7 @@ namespace ScintillaNet
 	{
 		#region Constants
 
-        public string DefaultDllName = IntPtr.Size == 4 ? "SciLexer.dll" : "SciLexer64.dll";
+		public const string DefaultDllName = "SciLexer.dll";
 
 		#endregion Constants
 
