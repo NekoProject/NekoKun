293766c857c947fb639d5d697f33964dd35f41d0
orzFly <wz.bluesnow@gmail.com>
2012-08-07 23:12:23 +0800

    MapInfoFile prototype...
    Signed-off-by: orzFly <wz.bluesnow@gmail.com>

---
+++ NekoKun/RPGMaker/Map/MapFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NekoKun.RPGMaker
+{
+    public class MapFile : AbstractFile
+    {
+        public string Title;
+
+        public MapFile(string filename)
+            : base(filename)
+        {
+            
+        }
+
+        protected override void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override AbstractEditor CreateEditor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NekoKun/RPGMaker/Map/MapInfoFile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NekoKun.RPGMaker
+{
+    public class MapInfoFile : AbstractFile
+    {
+        public Dictionary<string, MapFile> maps;
+
+        public MapInfoFile(Dictionary<string, object> node)
+            : base(
+                System.IO.Path.Combine(
+                    ProjectManager.ProjectDir,
+                    node["FileName"].ToString()
+                )
+              )
+        {
+            this.maps = new Dictionary<string, MapFile>();
+            RubyBindings.RubyHash mapHash = RubyBindings.RubyMarshal.Load(
+                new System.IO.FileStream(this.filename,  System.IO.FileMode.Open, System.IO.FileAccess.Read)
+            ) as RubyBindings.RubyHash;
+            foreach (var item in mapHash)
+	        {
+                string key = null;
+                string filename;
+                MapFile map = null;
+                if (item.Key is int)
+                {
+                    key = item.Key.ToString();
+                    filename = String.Format(node["FileNameFormat"].ToString(), (int)item.Key);
+                    map = new MapFile(System.IO.Path.Combine(ProjectManager.ProjectDir, filename));
+                }
+                else
+                {
+                    this.MakeDirty();
+                    continue;
+                }
+                this.maps.Add(key, map);
+
+                RubyBindings.RubyObject info = item.Value as RubyBindings.RubyObject;
+                map.Title = info["name"] as string;
+
+                /*
+                    parent_id 
+                    The parent map ID.
+
+                    order 
+                    The map tree display order, which is used internally.
+
+                    expanded 
+                    The map tree expansion flag, which is used internally.
+
+                    scroll_x 
+                    The x-axis scroll position, which is used internally.
+
+                    scroll_y 
+                    The y-axis scroll position, which is used internally.
+                */
+	        }
+            this.maps.ToString();
+        }
+
+        protected override void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override AbstractEditor CreateEditor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "地图索引";
+        }
+    }
+}
